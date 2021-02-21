ba44967ed9edcbc0ab45d611a31e516e7659b815
Richard Geoff St. John <rgeoffstjohn1@gmail.com>
2021-02-21 12:30:33 -0500

    Update BroadCollisionDetector.js

--- classes/BroadCollisionDetector.js
+++ classes/BroadCollisionDetector.js
@@ -1,7 +1,6 @@
 class BroadCollisionDetector {
 	constructor(universeSize) {
 		this.universeSize = universeSize
-
 		this.potentialCollisions = []
 		this.collisions = new CollisionList()
 	}
@@ -9,11 +8,9 @@ class BroadCollisionDetector {
 	findCollisions(mainArr, objArr) {
 		this.potentialCollisions = []
 		this.collisions.resetDetector()
-		//this.axisSweepX(objArr)
 		this.addAllPairs(mainArr, objArr)
 		this.addBBIntersectionPairs()
 		return this.collisions.resolveCollisions()
-		
 	}
 	
 	addAllPairs(mainArr, objArrays) {
@@ -27,18 +24,10 @@ class BroadCollisionDetector {
 		for (let i=0; i<mainArr.length; i++){
 			for (let j=0; j<objArrays.length; j++){
 				for (let k=0; k<objArrays[j].length; k++){
-					//console.log(objArrays[k])
-					//if (mainArr[i] != objArrays[j][k]){
-					
 						this.potentialCollisions.push({objOne:mainArr[i], objTwo:objArrays[j][k]})
-					//}
-					//if(mainArr[i].getId() === InteractiveObjID.player){
-					//	console.log("player")
-					//}
 				}
 			}
 		}
-		//console.log(this.potentialCollisions)
 	}
 	
 	//
@@ -49,7 +38,6 @@ class BroadCollisionDetector {
 		})
 		//console.log(objArr)
 		for(let i=0, n=objArr.length; i<n; i++) {
-			//this.potentialCollisions.push(objArr[i])
 			let j=i+1
 			if(j == n) {break}
 			
@@ -57,9 +45,7 @@ class BroadCollisionDetector {
 				this.potentialCollisions.push({objOne:objArr[i], objTwo:objArr[j]})
 				j++
 				if (j === n) {break}
-			}
-			//console.log(this.potentialCollisions)
-			
+			}			
 		}
 		
 	}
@@ -70,49 +56,20 @@ class BroadCollisionDetector {
 				this.collisions.addCollision(this.potentialCollisions[i].objOne, this.potentialCollisions[i].objTwo)
 			}
 		}
-		//this.potentialCollisions.forEach(function(coll) {
-		//	if(this.BBIntersect(coll.objOne, coll.objTwo)) {
-		//		this.collisions.addCollision(coll.objOne, coll.objTwo)
-		//	}
-		//}) 
 	}
 	
 	BBIntersect(obj1, obj2) {
-		//console.log("in BBIntersect")
-		
-		// This prevents 4 or more objects colliding simultaneously, use another method if you want to allow that case
-		
-		/*
-		if (obj1.isColliding() && obj2.isColliding) {
-			//if(obj1.isCollidingWith(obj2)){
-				return false
-			//}
-		}
-		*/
 		if (obj1.isFinishedColCalc() || obj2.isFinishedColCalc()){
 			return false
-		}
-		
+		}		
 		
 		let px = obj2.getOrigin().getX() - obj1.getOrigin().getX() - obj1.getBBLengthX()
 		let py = obj2.getOrigin().getY() - obj1.getOrigin().getY() - obj1.getBBLengthY()
 		let qx = obj1.getOrigin().getX() - obj2.getOrigin().getX() - obj2.getBBLengthX()
 		let qy = obj1.getOrigin().getY() - obj2.getOrigin().getY() - obj2.getBBLengthY()
-		
-		//if(obj1.getId() === InteractiveObjID.player){
-		//	console.log("player before")
-		//}
 
 		if(px > 0 || py > 0) { return false}
 		if(qx > 0 || qy > 0) { return false}
-		
-		//if(obj1.getId() === InteractiveObjID.player || obj2.getId() === InteractiveObjID.player){
-		//	console.log("player after")
-		//}
-		
-		//console.log("intersect: true")
-		
-		
 		
 		obj1.intersecting = true
 		obj2.intersecting = true
